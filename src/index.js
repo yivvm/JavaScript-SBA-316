7d7a43e20c3c89d25ed7f213536a9d4485697409
Yiwen Zhang <yiwenmary@gmail.com>
2024-02-24 15:06:04 -0500

    add 2 helper functions to simplify the code

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ topMenuEl.classList.add('flex-around');
 
 helperBuildMenu(menuLinks, topMenuEl);
 
-// helper function to build menu elements for all top-menu & sub-menu
+// -- helper function to build menu elements for all top-menu & sub-menu
 function helperBuildMenu(links, menuEl) {
     menuEl.innerHTML = "";
     links.forEach((link) => {
@@ -50,47 +50,42 @@ subMenuEl.classList.add('flex-around');
 subMenuEl.style.position = "absolute";
 subMenuEl.style.top = "0";
 
+// -- helper function for all addEventListener
+function addGlobalEventListener(element, type, selector, callback) {
+    element.addEventListener(type, e => {
+        e.preventDefault();
+        if (e.target.tagName !== 'A') { return; }
+
+        e.target.classList.toggle('active');
+
+        if (e.target.matches(selector)) callback(e)
+
+        topMenuLinks.forEach(function(link) {
+            link.classList.remove('active');
+        })
+    })
+} 
+
 // interaction between top-menu & sub-menu
 const topMenuLinks = topMenuEl.querySelectorAll('a');
-topMenuEl.addEventListener('click', function(event) {
-    const clickedEvent = event.target;
-    event.preventDefault();
-    if (clickedEvent.tagName !== 'A') { return; }
+addGlobalEventListener(topMenuEl, 'click', 'a', e => {
+    const hasSubLinks = menuLinks.find((link) => link.text === e.target.textContent).subLinks;
 
-    clickedEvent.classList.toggle('active');
-    topMenuLinks.forEach(function(link) {
-        link.classList.remove('active');
-    })
-
-    const hasSubLinks = menuLinks.find((link) => link.text === clickedEvent.textContent).subLinks;
-
-    if (clickedEvent.classList !== 'active') {
-        if (hasSubLinks) {
-            subMenuEl.style.top = "100%";
-            helperBuildMenu(hasSubLinks, subMenuEl);
-        } else {
-            subMenuEl.style.top = "0";
+        if (e.target.classList !== 'active') {
+            if (hasSubLinks) {
+                subMenuEl.style.top = "100%";
+                helperBuildMenu(hasSubLinks, subMenuEl);
+            } else {
+                subMenuEl.style.top = "0";
+            }
         }
-    }
 })
 
-// --- Todo: to build a function for all addEventListener
-// function helperAddEventListener(menuEl, ) {} 
-
-subMenuEl.addEventListener('click', function(event) {
-    const clickEvent = event.target;
-    event.preventDefault();
-    if (clickEvent.tagName !== 'A') { return; }
-
+addGlobalEventListener(subMenuEl, 'click', 'a', e => {
     if (subMenuEl.style.top === "100%") {
         subMenuEl.style.top = "0";
     }
-
-    topMenuLinks.forEach((link) => {
-        link.classList.remove('active');
-    })
 })
-
 
 
 // === ===============================
